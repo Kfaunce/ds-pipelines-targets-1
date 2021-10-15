a69199555c5d79a72e22f49ed592cc3522996140
Faunce <kfaunce@usgs.gov>
2021-10-15 10:09:00 -0400

    fix function names

--- code.R
+++ code.R
@@ -1,15 +1,15 @@
 # download_data
 
-source("1_fetch/src/getData.R")
+source("1_fetch/src/download_data.R")
 
 # process_data
 
-source("2_process/src/prepData.R")
+source("2_process/src/process_data.R")
 
 # make_plot
 
-source("3_visualize/src/plotData.R")
+source("3_visualize/src/make_plot.R")
 
 # generate_diagnostics
 
-source("2_process/src/saveDiagnostics.R")
+source("2_process/src/generate_diagnostics.R")
